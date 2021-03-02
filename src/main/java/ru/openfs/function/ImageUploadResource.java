cbc5f1da1a5aad296adf3aefbb6b34cba4dfb2bb
igor.m <igorm@openfs.ru>
2021-03-02 18:15:37 +0300

    change artifact name

--- src/main/java/ru/openfs/function/ImageUploadResource.java
+++ src/main/java/ru/openfs/function/ImageUploadResource.java
@@ -6,13 +6,14 @@ import java.net.URLDecoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.net.http.HttpRequest.BodyPublishers;
 import java.nio.charset.Charset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Random;
 import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -21,7 +22,6 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -43,12 +43,15 @@ import io.quarkus.grpc.runtime.annotations.GrpcService;
 import io.vertx.core.json.JsonObject;
 
 @Path("/")
-public class FileUploadResource {
-    static int OBJECT_NAME_LEN = 22;
-    Random random = new Random();
+public class ImageUploadResource {
+    static int PREFIX_LENGTH = 7;
+    static int NAME_LENGTH = 12;
 
     @ConfigProperty(name = "minio.bucket", defaultValue = "test")
     String bucket;
+
+    @ConfigProperty(name = "thumbnail.url", defaultValue = "http://localhost:8009")
+    URI thumbnailUrl;
 
     @Inject
     MinioClient minio;
@@ -58,7 +61,6 @@ public class FileUploadResource {
     DgraphGrpc.DgraphBlockingStub db;
 
     HttpClient client;
-    URI thumbnailUri = URI.create("http://83.68.33.151:8009");
 
     @PostConstruct
     void initialize() {
@@ -67,62 +69,39 @@ public class FileUploadResource {
 
     @POST
     @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response upload(MultipartFormDataInput body) throws Exception {
+    public Response uploadImage(MultipartFormDataInput body) throws Exception {
         String title = decodeTitle(body.getFormDataPart("title", String.class, null));
         InputPart fileInput = body.getFormDataMap().get("file").get(0);
+
         // generate objectName
         String object = createObjectName(fileInput.getMediaType().getSubtype());
-        // create db request
-        Set<NQuad> setNQuads = new HashSet<NQuad>();
-        // set type Image
-        setNQuads.add(NQuad.newBuilder().setSubject("_:image").setPredicate("dgraph.type")
-                .setObjectValue(Value.newBuilder().setStrVal("Image").build()).build());
-        // set title
-        setNQuads.add(NQuad.newBuilder().setSubject("_:image").setPredicate("Image.title")
-                .setObjectValue(Value.newBuilder().setStrVal(title).build()).build());
-        // set date (iso)
-        setNQuads.add(NQuad.newBuilder().setSubject("_:image").setPredicate("Image.date")
-                .setObjectValue(
-                        Value.newBuilder().setStrVal(ZonedDateTime.now().format(DateTimeFormatter.ISO_INSTANT)).build())
-                .build());
-        // set dt as usec
-        setNQuads.add(NQuad.newBuilder().setSubject("_:image").setPredicate("Image.dt")
-                .setObjectValue(Value.newBuilder().setIntVal(System.currentTimeMillis() / 1000).build()).build());
-        // add ImageSize reference
-        setNQuads.add(
-                NQuad.newBuilder().setSubject("_:image").setPredicate("Image.sizes").setObjectId("_:size").build());
-        // set type ImageSize
-        setNQuads.add(NQuad.newBuilder().setSubject("_:size").setPredicate("dgraph.type")
-                .setObjectValue(Value.newBuilder().setStrVal("ImageSize").build()).build());
-        // set image type orig
-        setNQuads.add(NQuad.newBuilder().setSubject("_:size").setPredicate("ImageSize.type")
-                .setObjectValue(Value.newBuilder().setStrVal("og").build()).build());
-        // set image path (bucket + object)
-        setNQuads.add(NQuad.newBuilder().setSubject("_:size").setPredicate("ImageSize.path")
-                .setObjectValue(Value.newBuilder().setStrVal(bucket + '/' + object).build()).build());
+
         // store to db
-        Map<String, String> uids = db.query(Request.newBuilder()
-                .addMutations(Mutation.newBuilder().addAllSet(setNQuads).build()).setCommitNow(true).build())
+        Map<String, String> uids = db
+                .query(Request.newBuilder().addMutations(setImage(bucket, object, title)).setCommitNow(true).build())
                 .getUidsMap();
-        // put file to object store with tag fileid
+
+        // put file to object store
         minio.putObject(PutObjectArgs.builder().bucket(bucket).object(object).tags(uids)
                 .contentType(fileInput.getMediaType().getType() + "/" + fileInput.getMediaType().getSubtype())
                 .stream(fileInput.getBody(InputStream.class, null), -1, 5 * 1024 * 1024).build());
+        body.close();
+
         // call thumbnail
-        String thumbnailJson = String.format("{\"Key\":\"%s/%s\"}", bucket, object);
-        var response = client.send(
-                HttpRequest.newBuilder(thumbnailUri).header("Content-type", "application/json")
-                        .POST(HttpRequest.BodyPublishers.ofString(thumbnailJson)).build(),
-                HttpResponse.BodyHandlers.ofString());
+        String thumbnailJson = String.format(
+                "{ \"bucket\":\"%s\", \"object\":\"%s\", \"imageUid\":\"%s\", \"imageSizeUid\": \"%s\"}", bucket,
+                object, uids.get("image"), uids.get("size"));
+        var response = client.send(HttpRequest.newBuilder(thumbnailUrl).header("Content-type", "application/json")
+                .POST(BodyPublishers.ofString(thumbnailJson)).build(), HttpResponse.BodyHandlers.ofString());
         if (response.statusCode() == 200) {
             return Response.ok("ok").build();
         }
-        return Response.serverError().build();
+        return Response.status(Response.Status.BAD_GATEWAY).entity("Can not store file to cloud").build();
     }
 
     @DELETE
     @Path("/{id}")
-    public void deleteFile(@PathParam("id") String id) throws Exception {
+    public void deleteImage(@PathParam("id") String id) throws Exception {
         // get file path list
         String pathJson = db.query(Request.newBuilder()
                 .setQuery(String.format("{dd(func: uid(%s)) { Image.sizes { ImageSize.path }}}", id)).setReadOnly(true)
@@ -147,16 +126,47 @@ public class FileUploadResource {
                 .setCommitNow(true).build());
     }
 
+    private Mutation setImage(String bucket, String object, String title) {
+        Set<NQuad> setNQuads = new HashSet<NQuad>();
+        setNQuads.add(NQuad.newBuilder().setSubject("_:image").setPredicate("dgraph.type")
+                .setObjectValue(Value.newBuilder().setStrVal("Image").build()).build());
+        setNQuads.add(NQuad.newBuilder().setSubject("_:image").setPredicate("Image.title")
+                .setObjectValue(Value.newBuilder().setStrVal(title).build()).build());
+        setNQuads.add(NQuad.newBuilder().setSubject("_:image").setPredicate("Image.date")
+                .setObjectValue(
+                        Value.newBuilder().setStrVal(ZonedDateTime.now().format(DateTimeFormatter.ISO_INSTANT)).build())
+                .build());
+        setNQuads.add(NQuad.newBuilder().setSubject("_:image").setPredicate("Image.dt")
+                .setObjectValue(Value.newBuilder().setIntVal(System.currentTimeMillis() / 1000).build()).build());
+        setNQuads.add(
+                NQuad.newBuilder().setSubject("_:image").setPredicate("Image.sizes").setObjectId("_:size").build());
+        setNQuads.add(NQuad.newBuilder().setSubject("_:size").setPredicate("dgraph.type")
+                .setObjectValue(Value.newBuilder().setStrVal("ImageSize").build()).build());
+        setNQuads.add(NQuad.newBuilder().setSubject("_:size").setPredicate("ImageSize.type")
+                .setObjectValue(Value.newBuilder().setStrVal("og").build()).build());
+        setNQuads.add(NQuad.newBuilder().setSubject("_:size").setPredicate("ImageSize.path")
+                .setObjectValue(Value.newBuilder().setStrVal(bucket + '/' + object).build()).build());
+        return Mutation.newBuilder().addAllSet(setNQuads).build();
+    }
+
     private void deleteFilePath(String filePath) throws Exception {
-        String bucket = filePath.split("/")[0];
-        String filename = filePath.split("/")[1];
-        minio.removeObject(RemoveObjectArgs.builder().bucket(bucket).object(filename).build());
+        int sep = filePath.indexOf("/");
+        String bucket = filePath.substring(0, sep);
+        String object = filePath.substring(sep + 1);
+        minio.removeObject(RemoveObjectArgs.builder().bucket(bucket).object(object).build());
     }
 
     private String createObjectName(String suffix) {
-        return random.ints(48, 122).filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97)).limit(OBJECT_NAME_LEN)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString() + "."
-                + suffix;
+        String generatedString = ThreadLocalRandom.current().ints(48, 122)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97)).limit(NAME_LENGTH)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+        return createPrefix() + generatedString + "." + suffix;
+    }
+
+    private String createPrefix() {
+        String generatedString = ThreadLocalRandom.current().ints(48, 58).limit(PREFIX_LENGTH)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+        return "x" + generatedString + "/";
     }
 
     private String decodeTitle(String title) {
